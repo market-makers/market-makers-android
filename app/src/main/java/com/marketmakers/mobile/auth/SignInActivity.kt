f148410dec54d5b496383acc935b70641498a055
Gabriel de Lira Ferreira <liraferreiragabriel@gmail.com>
2018-08-18 18:55:02 -0300

    adding login and home screen

---
+++ app/src/main/java/com/marketmakers/mobile/auth/SignInActivity.kt
@@ -0,0 +1,51 @@
+package com.marketmakers.mobile.auth
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.marketmakers.mobile.R
+import com.google.firebase.auth.FirebaseAuth
+import com.marketmakers.mobile.promotions.PromotionsActivity
+import java.util.*
+
+
+class SignInActivity : AppCompatActivity() {
+
+    private val rcSignIn = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                rcSignIn)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == rcSignIn) {
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser ?: return
+
+                val intent = Intent(applicationContext, PromotionsActivity::class.java)
+                intent.putExtra(PromotionsActivity.EXTRA_CURRENT_USER, user.uid)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this,  "Ops, algo deu errado!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
